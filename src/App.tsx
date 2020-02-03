a56ce5e6b25426325499874a057d3fa69a8e6701
Raymond Sotto <raymondjosephsotto@gmail.com>
2020-02-02 22:19:48 -0500

    Added Folder Structure, Hardcoded Mockup Layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,18 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="App-welcomeSection">
+       <div className="App-nav">Nav Icon</div>
+       <div className="App-header">
+         <h1>Hi Samantha</h1>
+         <p>Welcome back to the workspace we missed you</p>
+       </div>
+       <div className="App-searchBar">
+         searchbar
+       </div>
+         <div className="App-projects">Project Section</div>
+       </div>
+       <div className="App-resultSection">Cyberpunk</div>
     </div>
   );
 }
