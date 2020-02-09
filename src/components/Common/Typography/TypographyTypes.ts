1d7a446402255c115928d625002745bdbc672a6a
codyarose <crose992@gmail.com>
2020-02-09 14:03:38 -0600

    Added Typography component and h1-6, p styles

---
+++ src/components/Common/Typography/TypographyTypes.ts
@@ -0,0 +1,25 @@
+interface TypoVariant {
+	variant?: 'h1'
+	| 'h2'
+	| 'h3'
+	| 'h4'
+	| 'h5'
+	| 'h6'
+	| 'p'
+}
+interface TypoAlign {
+	align?: 'inherit'
+	| 'left'
+	| 'center'
+	| 'right'
+	| 'justify'
+}
+interface TypoColor {
+	color?: string
+}
+interface Props {
+	children: string
+	component?: any
+}
+
+export default interface TypographyTypes extends TypoVariant, TypoAlign, TypoColor, Props { }
---
+++ src/components/Common/Typography/index.tsx
@@ -0,0 +1,94 @@
+import React, { FC } from 'react'
+import styled, { css } from 'styled-components'
+import TypographyTypes from './TypographyTypes'
+
+const getComponent = (variant: string) => {
+	switch (variant) {
+		case 'h1':
+		case 'h2':
+		case 'h3':
+		case 'h4':
+		case 'h5':
+		case 'h6':
+			return 'h2'
+		default:
+			return variant
+	}
+}
+
+const Typography: FC<TypographyTypes> = ({
+	variant = 'p',
+	component,
+	align = 'inherit',
+	color = 'inherit',
+	children
+}: TypographyTypes) => {
+	return (
+		<StyledTypography
+			variant={variant}
+			as={component ? component : getComponent(variant)}
+			align={align}
+			color={color}
+		>
+			{children}
+		</StyledTypography>
+	)
+}
+
+export default Typography
+
+const BaseStyles = css`
+	font-weight: normal;
+`
+
+const BaseHeaderStyles = css`
+	${BaseStyles}
+	line-height: 1.2;
+`
+
+const H1Styles = css`
+	${BaseHeaderStyles}
+	font-size: 40px;
+`
+const H2Styles = css`
+	${BaseHeaderStyles}
+	font-size: 30px;
+`
+
+const H3Styles = css`
+	${BaseHeaderStyles}
+	font-size: 25px;
+`
+
+const H4Styles = css`
+	${BaseHeaderStyles}
+	font-size: 20px;
+`
+
+const H5Styles = css`
+	${BaseHeaderStyles}
+	font-size: 18px;
+`
+
+const H6Styles = css`
+	${BaseHeaderStyles}
+	font-size: 16px;
+`
+
+const PStyles = css`
+	${BaseStyles}
+	font-size: 15px;
+	line-height: 1.6;
+`
+
+const StyledTypography = styled.div<TypographyTypes>`
+	text-align: ${({ align }) => align};
+	color: ${({ color }) => color};
+	${({ variant }) => variant === 'h1' && H1Styles}
+	${({ variant }) => variant === 'h2' && H2Styles}
+	${({ variant }) => variant === 'h3' && H3Styles}
+	${({ variant }) => variant === 'h4' && H4Styles}
+	${({ variant }) => variant === 'h5' && H5Styles}
+	${({ variant }) => variant === 'h6' && H6Styles}
+	${({ variant }) => variant === 'p' && PStyles}
+`
