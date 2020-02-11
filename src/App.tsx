bc9192ef651a067d4d38f5b3cff69809e36c1129
Rob Martin <rmartin3@alumni.unca.edu>
2020-02-10 16:31:25 -0800

    Paper and Elements folder

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,24 @@ const app_theme = {
   base: "#2b2e43",
   paper: "#2f3247",
   contrast: "#956ee5",
-  pattern: null,
   font: "white",
-  fontContrast: "#4c4f61",
-  modified: null
+  fontContrast: "#4c4f61"
 };
 
-const App = () => (
-  <ThemeProvider theme={app_theme}>
-    <Canvas>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Projects} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contacts" exact component={Contacts} />
-          <Route path="/settings" exact component={Settings} />
-        </Switch>
-      </Router>
-    </Canvas>
-  </ThemeProvider>
-);
-
+const App = () => {
+  return (
+    <ThemeProvider theme={app_theme}>
+      <Canvas>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Projects} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contacts" exact component={Contacts} />
+            <Route path="/settings" exact component={Settings} />
+          </Switch>
+        </Router>
+      </Canvas>
+    </ThemeProvider>
+  );
+};
 export default App;
