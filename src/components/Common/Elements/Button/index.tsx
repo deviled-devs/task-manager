557228ea1e55d8dce88978b32cc3c07725eba0a3
Rob Martin <rmartin3@alumni.unca.edu>
2020-02-12 13:53:59 -0800

    code review changes.

--- src/components/Common/Elements/Button/index.tsx
+++ src/components/Common/Elements/Button/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import styled from "styled-components";
 
 interface Props {
-  children: any;
+  children: ReactNode;
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
