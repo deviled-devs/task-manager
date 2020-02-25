cbfaeef1a5352a3fdeb0fadf421626336a4d1277
Raymond Sotto <raymondjosephsotto@gmail.com>
2020-02-25 15:39:42 -0500

    Organized the Components, deleted <div> tag and useState

--- src/components/Projects/Welcome/Welcome.js
+++ src/components/Projects/Welcome/Welcome.js
@@ -1,30 +1,24 @@
-import React, {useState} from "react";
+import React from "react";
 import styled from "styled-components";
+import Typography from '../../Common/Elements/Typography/index';
 
-
+const Welcome = () => {
+  return (
+    <Layout>
+        <h2>Hi Samantha</h2>
+        <StyledTypography>Welcome back to the workspace, we missed you!</StyledTypography> 
+    </Layout>
+  )
+}
 
 const Layout = styled.div`
   display: grid;
   grid-gap: 1em;
 `;
 
-const P = styled.p`
+const StyledTypography = styled(Typography)`
     color: #8A8C98;
 `;
 
-
-const Welcome = () => {
-    const [person, setPerson] = useState({name: 'Samantha' });
-
-
-    return (
-      <Layout>
-          <div>
-          <h2>Hi {person.name}</h2>
-          <P>Welcome back to the workspace, we missed you!</P>
-          </div>
-      </Layout>
-    )
-}
 
 export default Welcome;
